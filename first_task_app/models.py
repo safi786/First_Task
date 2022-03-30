51c5e5c8bcb6c3fcf33e147d25750fafef5313b5
SAFI UR REHMAN KHAN <safiurrehmankhan4@gmail.com>
2022-03-30 22:34:13 +0500

    now branch added

--- first_task_app/models.py
+++ first_task_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class StudentClub(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False,)
+    email = models.EmailField(max_length=100)
+    mobile = models.CharField(max_length=50)
+    landLine = models.CharField(max_length=50)
+    address = models.TextField(max_length=50)
+    city = models.CharField(max_length=50)
