6ad3956cfa9ec5cf729a7360e81666c12bb2b561
SAFI UR REHMAN KHAN <safiurrehmankhan4@gmail.com>
2022-04-16 21:40:20 +0500

    updated

--- FirstTask/settings.py
+++ FirstTask/settings.py
@@ -137,7 +137,7 @@ django_heroku.settings(locals())
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 MAILER_EMAIL_BACKEND = EMAIL_BACKEND
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = 'Ekhetiger.4365'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_HOST_USER = 'safiurrehmankhan6@gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
